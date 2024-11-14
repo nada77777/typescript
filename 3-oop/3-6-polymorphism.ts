310f05f0d59743832da3fdcaf2be8c4e758d903f
ksh17 <rkstkd98@naver.com>
2024-11-14 20:49:19 +0900

    다형성

---
+++ 3-oop/3-6-polymorphism.ts
@@ -0,0 +1,111 @@
+{
+  // 다형성 학습
+  type CoffeeCup = {
+    shots: number;
+    hasMilk?: boolean;
+    hasSugar?: boolean;
+  };
+
+  // CoffeeMaker 인터페이스 만들어
+  interface CoffeeMaker {
+    makeCoffee(shots: number): CoffeeCup;
+  }
+
+  class CoffeeMachine implements CoffeeMaker {
+    private static BEANS_GRAMM_PER_SHOT: number = 7; // class level
+    private coffeeBeans: number = 0; // instance (object) level
+
+    constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    static makeMachine(coffeeBeans: number): CoffeeMachine {
+      return new CoffeeMachine(coffeeBeans);
+    }
+
+    fillCoffeeBeans(beans: number) {
+      if (beans < 0) {
+        throw new Error("value for beans should be greater than 0");
+      }
+      this.coffeeBeans += beans;
+    }
+
+    clean() {
+      console.log("cleaning the machine...🧼");
+    }
+
+    private grindBeans(shots: number) {
+      console.log(`grinding beans for ${shots}`);
+      if (this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT) {
+        throw new Error("Not enough coffee beans!");
+      }
+      this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT;
+    }
+
+    private preheat(): void {
+      console.log("heating up... 🔥");
+    }
+
+    private extract(shots: number): CoffeeCup {
+      console.log(`Pulling ${shots} shots... ☕️`);
+      return {
+        shots,
+        hasMilk: false,
+      };
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      this.grindBeans(shots);
+      this.preheat();
+      return this.extract(shots);
+    }
+  }
+
+  // CaffeLatteMachine 클래스 만들어 CoffeeMachine 상속받아
+  class CaffeLatteMachine extends CoffeeMachine {
+    constructor(beans: number, public readonly serialNumber: string) {
+      super(beans);
+    }
+    private steamMilk(): void {
+      console.log("Steaming some milk... 🥛");
+    }
+    makeCoffee(shots: number): CoffeeCup {
+      const coffee = super.makeCoffee(shots);
+      this.steamMilk();
+      return {
+        ...coffee,
+        hasMilk: true,
+      };
+    }
+  }
+
+  // SweetCoffeeMaker 클래스 만들어 CoffeeMachine 상속받아
+  class SweetCoffeeMaker extends CoffeeMachine {
+    makeCoffee(shots: number): CoffeeCup {
+      const coffee = super.makeCoffee(shots);
+      return {
+        ...coffee,
+        hasSugar: true,
+      };
+    }
+  }
+
+  //  CaffeLatteMachine, SweetCoffeeMaker 클래스 둘 다 CoffeeMachine을 상속을 받았지
+  //  그런데 CaffeLatteMachine 클래스에는 steamMilk 함수가 존재, SweetCoffeeMaker 클래스에는 steamMilk 함수가 없어 똑같이 상속 받았어도?
+  //  지금 나오는 결과물이 달라(hasMilk, hasSugar)
+
+  const machines: CoffeeMaker[] = [
+    new CoffeeMachine(16),
+    new CaffeLatteMachine(16, "1"),
+    new SweetCoffeeMaker(16),
+    new CoffeeMachine(16),
+    new CaffeLatteMachine(16, "1"),
+    new SweetCoffeeMaker(16),
+  ];
+  machines.forEach((machine) => {
+    console.log("-------------------------");
+    machine.makeCoffee(1);
+  });
+
+  const aa = new CaffeLatteMachine(16, "1");
+}
